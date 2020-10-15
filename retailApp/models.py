089c9a6b68aa458886ab09064dd8300ef580e8ed
Lehlohonolo <lehramaano@gmail.com>
2020-10-15 15:18:57 +0200

    Image features

--- retailApp/models.py
+++ retailApp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class Product(models.Model):
-    product_id = models.CharField(max_length=100)
     name = models.CharField(max_length = 200)
+    image = models.ImageField(upload_to='product', null=True, blank=True)
     price = models.FloatField()
     description = models.TextField()
     category = models.CharField(max_length = 200)
